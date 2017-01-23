6025f178bca3d66e4d9c782f3b4d79e278d04909
John Mestebeld <John Mestebeld>
2017-01-23 10:51:16 +0100

    upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+
+    
+<form method="post" action="">
+    Naam:<input type="text" name="naam" value="" />
+    <br><br>
+    Kleur:<input type="text" name="kleur" value="" />
+    <br><br><input type="submit" name="submit" />
+  </form>
+    
+<?php
+    require "formulier.php";
+?>
+    
+</body>
+</html>
